28f63af56ef1e080ef4009bc54ccb8e0dfee8343
MarekAniol <marekaniol1@gmail.com>
2022-03-03 21:10:31 +0000

    Return Tuple

--- dlugosci_max_min_flag.py
+++ dlugosci_max_min_flag.py
@@ -3,7 +3,8 @@ from lista_flag import stworz_liste_flag
 
 def get_min_max_domain(lst):
     sorted_flags = sorted(lst, key=len)
-    return sorted_flags[0], sorted_flags[-1]
+    domains_with_lenght = [(len(d), d) for d in sorted_flags]
+    return domains_with_lenght[0], domains_with_lenght[-1] # sorted_flags[0], sorted_flags[-1]
 
 
 url = 'https://zajecia-programowania-xd.pl/flagi'
